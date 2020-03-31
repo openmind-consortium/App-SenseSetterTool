9ab4efd2eaa8f40afa04132a5212f1daadc19534
Randy Perrone <mrrandyperrone@gmail.com>
2020-03-31 15:26:48 -0700

    Imported classes from AdaptiveDBS code.  Ironed out all the errors and fixed some issues with requiring Medtronic libraries.

---
+++ UCSF-StarrLab-SenseSetter/UCSF-StarrLab-SenseSetter/Services/ConfigConversions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCSF_StarrLab_SenseSetter.Services
+{
+    class ConfigConversions
+    {
+    }
+}
